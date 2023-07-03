16341848644f0019e95aa01fada643f81fd0f9a9
Agustinll03 <127960515+Agustinll03@users.noreply.github.com>
2023-07-03 18:44:13 -0300

    terminado

--- TRABAJO/contacto.js
+++ TRABAJO/contacto.js
@@ -2,7 +2,6 @@
  * Deshabilita el comportamiento predeterminado del envío del formulario y previene que la página se recargue.
  * @method prevenirEnvioFormulario
  */
-
 document
     .getElementById("miFormulario2")
     .addEventListener("submit", function (event) {
@@ -13,7 +12,6 @@ document
          * @method obtenerValoresFormulario
          * @return {Object} - Un objeto con las propiedades `nombre`, `email`,  `asunto` y `mensaje` que contienen los valores ingresados en los campos del formulario.
          */
-
         let nombre = document.getElementById("nombre").value;
         let mensaje2 = document.getElementById("mensaje2").value;
         let email = document.getElementById("email").value;
@@ -27,7 +25,6 @@ document
          * @param {string} asunto - El asunto ingresado en el formulario.
          * @param {string} mensaje - El mensaje ingresado en el formulario.
          */
-
         if (nombre && email && asunto && mensaje2) {
             if (/^[a-zA-Z]+$/.test(nombre)) {
                 alert("¡Muchas gracias por completar el formulario!");
