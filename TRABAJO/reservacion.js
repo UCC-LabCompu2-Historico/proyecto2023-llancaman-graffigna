d5e5b9dd445af9ab73efa4ace84b59986ea8a8df
Agustinll03 <127960515+Agustinll03@users.noreply.github.com>
2023-07-04 22:36:34 -0300

    terminado

--- TRABAJO/reservacion.js
+++ TRABAJO/reservacion.js
@@ -1,90 +1,40 @@
 /**
- * Ejecuta una función cuando el contenido del documento ha sido completamente cargado.
- * @method addEventListener
- * @param {string} evento - El evento al que se va a suscribir ("DOMContentLoaded" en este caso).
- * @param {function} callback - La función que se ejecutará cuando se dispare el evento.
+ * Deshabilita el comportamiento predeterminado del envío del formulario y previene que la página se recargue.
+ * @method prevenirEnvioFormulario
  */
-document.addEventListener("DOMContentLoaded", function () {
-    /**
-     * Obtiene el elemento del documento con el ID "miFormulario".
-     * @method getElementById
-     * @param {string} id - El ID del elemento a buscar ("miFormulario" en este caso).
-     * @return {object} Retorna el elemento del documento con el ID especificado.
-     */
-    let formulario = document.getElementById("miFormulario");
+document.getElementById("miFormulario").addEventListener("submit", function (event) {
+    event.preventDefault();
 
     /**
-     * Obtiene el elemento del documento con el ID "telefono".
-     * @method getElementById
-     * @param {string} id - El ID del elemento a buscar ("telefono" en este caso).
-     * @return {object} Retorna el elemento del documento con el ID especificado.
+     * Obtiene los valores ingresados en los campos del formulario.
+     * @method obtenerValoresFormulario
+     * @return {Object} - Un objeto con las propiedades `nombre`, `email`,  `asunto` y `mensaje` que contienen los valores ingresados en los campos del formulario.
      */
-    let telefonoInput = document.getElementById("telefono");
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let tiempo = document.getElementById("time").value;
+    let telefono = document.getElementById("telefono").value;
 
     /**
-     * Obtiene el elemento del documento con el ID "nombre".
-     * @method getElementById
-     * @param {string} id - El ID del elemento a buscar ("nombre" en este caso).
-     * @return {object} Retorna el elemento del documento con el ID especificado.
+     * Valida y maneja el envío del formulario.
+     * @method validarEnvioFormulario
+     * @param {string} nombre - El nombre ingresado en el formulario.
+     * @param {string} email - El correo electrónico ingresado en el formulario.
+     * @param {string} tiempo - El asunto ingresado en el formulario.
+     * @param {string} telefono - El mensaje ingresado en el formulario.
      */
-    let nombreInput = document.getElementById("nombre");
-
-    /**
-     * Escucha el evento de envío del formulario y realiza validaciones.
-     * @method addEventListener
-     * @param {string} evento - El evento al que se va a suscribir ("submit" en este caso).
-     * @param {function} callback - La función que se ejecutará cuando se dispare el evento.
-     */
-    formulario.addEventListener("submit", function (event) {
-        if (formulario.checkValidity()) {
-            /**
-             * Obtiene el valor del campo de entrada de teléfono.
-             * @property value
-             * @type {string} Valor actual del campo de entrada.
-             */
-            let telefono = telefonoInput.value;
-
-            /**
-             * Obtiene el valor del campo de entrada de nombre.
-             * @property value
-             * @type {string} Valor actual del campo de entrada.
-             */
-            let nombre = nombreInput.value;
-
-            if (!esNombreValido(nombre)) {
-                event.preventDefault();
-                alert("El nombre debe contener solo letras.");
-                nombreInput.focus();
-            } else if (!esNumeroTelefono(telefono)) {
-                event.preventDefault();
-                alert("El número de teléfono debe contener solo números.");
-                telefonoInput.focus();
+    if (nombre && email && tiempo && telefono) {
+        if (/^[a-zA-Z]+$/.test(nombre)) {
+            if (/^[0-9]+$/.test(telefono)) {
+                alert("¡Muchas gracias por completar el formulario!");
+                document.getElementById("miFormulario").reset();
             } else {
-                alert("Gracias por completar el formulario");
-                formulario.reset();
+                alert("Por favor, ingresa solo números en el campo telefono.");
             }
+        } else {
+            alert("Por favor, ingresa solo letras en el campo nombre.");
         }
-    });
-
-    /**
-     * Verifica si un campo dado consiste únicamente en dígitos numéricos.
-     * @method esNumeroTelefono
-     * @param {string} numero - El número a verificar.
-     * @return {boolean} Retorna true si el número consiste únicamente en dígitos numéricos, de lo contrario retorna false.
-     */
-    function esNumeroTelefono(numero) {
-        let nume = /^[0-9]+$/;
-        return nume.test(numero);
-    }
-
-    /**
-     * Verifica si un campo dado consiste únicamente en letras.
-     * @method esNombreValido
-     * @param {string} nombre - El nombre a verificar.
-     * @return {boolean} Retorna true si el nombre consiste únicamente en letras, de lo contrario retorna false.
-     */
-    function esNombreValido(nombre) {
-        let letras = /^[A-Za-z]+$/;
-        return letras.test(nombre);
+    } else {
+        alert("Por favor, completa todos los campos del formulario.");
     }
 });
